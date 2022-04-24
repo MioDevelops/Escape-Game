f763f35bcf52b5ce74b003fb0fde51861e2418a3
Mio <MioDevelops@gmail.com>
2022-04-23 22:20:28 -0400

    f

--- main.py
+++ main.py
@@ -6,51 +6,34 @@ class Game():
         self.background = (100, 0, 150)
         self.running = True
         self.screen = pygame.display.set_mode([500,400])
-
-    def player(self):
-        png = pygame.image.load("ball.png")
-        png = pygame.transform.smoothscale(png, (40, 40))
-        ball = png.get_rect()
-        return png, ball
-
-    def move_left(self):
-        pass
-
-    def move_right(self):
-        pass
-
-    def move_up(self):
-        print("moving")
-        player = self.player()
-        player[1].move(0, 1)
-
-    def move_down(self):
-        pass
+        self.player = pygame.rect.Rect((64, 54, 25, 25))
 
     def __update__(self):
-        game.screen.fill(self.background)
-        game.screen.blit(self.player()[0], self.player()[1])
+        self.screen.fill(self.background)
+        pygame.draw.rect(self.screen, (0, 0, 0), self.player)
 
-class Obstacle():
-    def __init__(self):
-        self.list = []
-
-    def create_obstacle(self, x, y, width, height, img):
-        print(x, y, width, height, img)
-        img = pygame.image.load(img)
-        img = pygame.transform.smoothscale(img, (width, height))
-        obstacle = img.get_rect()
-        self.list.append([img, obstacle])
+#class Obstacle():
+#    def __init__(self):
+#        self.list = []
+#
+#    def create_obstacle(self, x, y, width, height, img):
+#        print(x, y, width, height, img)
+#        img = pygame.image.load(img)
+#        img = pygame.transform.smoothscale(img, (width, height))
+#        obstacle = img.get_rect()
+#        self.list.append([img, obstacle])
+#obstacles = Obstacle()
+#obstacles.create_obstacle(200, 100, 30, 30, "ball2.png")
 
 game = Game()
-obstacles = Obstacle()
-
-obstacles.create_obstacle(200, 100, 30, 30, "ball2.png")
 while game.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: exit()
         keys = pygame.key.get_pressed()
-        if(keys[pygame.K_w]): game.move_up()
+        if(keys[pygame.K_w]): game.player.move_ip(0, -2)
+        elif(keys[pygame.K_a]): game.player.move_ip(-2, 0)
+        elif(keys[pygame.K_s]): game.player.move_ip(0, 2)
+        elif(keys[pygame.K_d]): game.player.move_ip(2, 0)
 
     game.__update__()
     pygame.display.flip()
